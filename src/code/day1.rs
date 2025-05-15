8b5a35f134513c49f7a40d80b8252b14d88b3783
ruangsak.dev <104247036+0xRuangsak@users.noreply.github.com>
2025-05-14 23:49:32 -0400

    update the day 1 structure

--- src/code/day1.rs
+++ src/code/day1.rs
@@ -1,56 +1,48 @@
-use std::collections::HashMap;
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
-fn read_lines(file: &str) -> Vec<String> {
-    let mut result: Vec<String> = Vec::new();
-    for line in read_to_string(file).unwrap().lines() {
-        result.push(line.to_string());
-    }
-    result
-}
+fn main() {
+    let file_vector: Vec<String> = read_to_string("input")
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect();
 
-fn load_file(string_vector: Vec<String>) -> (Vec<i32>, Vec<i32>) {
-    let mut vec1: Vec<i32> = Vec::new();
-    let mut vec2: Vec<i32> = Vec::new();
-
-    for element in string_vector.iter() {
-        let parts: Vec<&str> = element.split_whitespace().collect();
-        vec1.push(parts[0].parse::<i32>().unwrap());
-        vec2.push(parts[1].parse::<i32>().unwrap());
-    }
-
-    (vec1, vec2)
-}
-
-pub fn solve_day1_part1() -> i32 {
-    let lines: Vec<String> = read_lines("src/input/day1.txt");
-    let (mut left, mut right) = load_file(lines);
-
-    left.sort();
-    right.sort();
-
-    let result: i32 = left
+    let mut file_tuple: (Vec<i32>, Vec<i32>) = file_vector
         .iter()
-        .zip(right.iter())
-        .map(|(a, b)| (a - b).abs())
-        .sum();
+        .map(|line| {
+            let mut parts = line.split_whitespace();
+            (
+                parts.next().unwrap().parse::<i32>().unwrap(),
+                parts.next().unwrap().parse::<i32>().unwrap(),
+            )
+        })
+        .unzip();
 
-    result
-}
+    fn part1(mut file_tuple: &mut (Vec<i32>, Vec<i32>)) -> i32 {
+        file_tuple.0.sort();
+        file_tuple.1.sort();
 
-pub fn solve_day1_part2() -> i32 {
-    let lines: Vec<String> = read_lines("src/input/day1.txt");
-    let (left, right) = load_file(lines);
-
-    let mut counts = HashMap::new();
-    for num in right {
-        *counts.entry(num).or_insert(0) += 1;
+        file_tuple
+            .0
+            .iter()
+            .zip(file_tuple.1.iter())
+            .map(|(a, b)| (a - b).abs())
+            .sum()
     }
 
-    let mut result: i32 = 0;
-    for num in left {
-        let count = counts.get(&num).unwrap_or(&0);
-        result += num * *count;
+    println!("Part 1 Answer : {}", part1(&mut file_tuple));
+
+    fn part2(mut file_tuple: &mut (Vec<i32>, Vec<i32>)) -> i32 {
+        let mut counts = HashMap::new();
+        for &num in &file_tuple.1 {
+            *counts.entry(num).or_insert(0) += 1;
+        }
+        let mut results: i32 = 0;
+        for &num in &file_tuple.1 {
+            results += num * counts[&num];
+        }
+        results
     }
-    result
+
+    println!("Part 2 Answer : {}", part2(&mut file_tuple));
 }
